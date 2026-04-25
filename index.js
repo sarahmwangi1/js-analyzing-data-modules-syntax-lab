0a96543fb5c8fda1d84f6a8b7426a1500144f4cc
sarah.mwangi1@student.moringaschool.com <snmwangi88@gmail.com>
2026-04-25 20:00:52 +0300

    analyzing-data-modules-syntax-lab

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
+// Import datejs
+require ("datejs");
 
+//  Initialize the Return Object with empty users array
+function combineUsers (...args) {
+   const combinedObject = {
+     users: []
+   };
 
+// Loop through args and merge each array into users
+args.forEach((userArray) => {
+  combinedObject.users.push (...userArray);
+});
 
-module.exports = {
-  ...(typeof combineUsers !== 'undefined' && { combineUsers })
-};
+// Add today's date in M/d/yyyy format
+combinedObject.merge_date = new date ().toString ("M/d/yyyy");
+
+// Return the combined object
+return combinedObject;
+}
+
+module.exports = { combineUsers };
